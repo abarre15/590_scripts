f3c20b4489d341b7eb67303b1bd23785b78403d3
Abby Barrett <abbybarrett@Abbys-MacBook-Air.local>
2017-02-08 15:59:13 -0500

    test move

---
+++ R/task01_script.R
@@ -0,0 +1,28 @@
+#####
+# task 1 in R for geol 590
+#####
+
+#load packages 
+library(tidyverse)
+
+x<-seq(from =1, to =20, length.out =10)
+y<-rnorm(10)
+labels<-letters[10]
+
+d<-data.frame(x, y, labels)
+write.csv(d, "data/test_data.csv")
+
+plot(x,y)
+png("plots/test_plot.png", height =3, width = 4, units = "in", res = 300)
+
+#dev.off()
+
+#save.csv
+write.csv("data/test_data.csv")
+
+
+my.variable <- 2+2
+q()
+
+my.variable/3
+q()
